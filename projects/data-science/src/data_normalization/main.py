bec792392f4fd33517cbd557152fcf6fb09ac970
odarbr <oda.ryland.brun@gmail.com>
2025-06-06 16:39:48 +0200

    Data normalization

--- projects/data-science/src/data_normalization/main.py
+++ projects/data-science/src/data_normalization/main.py
@@ -9,11 +9,13 @@ def data_normalization(csv_path: str) -> pd.DataFrame:
         pd.DataFrame: Normalized DataFrame
     """
 
-    # Read with correct seperator and encoding
-    df = pd.read_csv(csv_path, sep=";", encoding="latin1")
+    # Define column names
+    columns = ["statistikkvariabel", "region", "kjønn", "alder",
+    "2011", "2012", "2013", "2014", "2015", "2016",
+    "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024"]
 
-    # Clean column names
-    df.columns = [col.strip().replace("�", "ø").replace(" ", "_").lower() for col in df.columns]
+    # Read with correct seperator and encoding
+    df = pd.read_csv(csv_path, sep="\t", quotechar='"', names=columns, header=None, skiprows=1, encoding="utf-8")
 
     # Melt broad data into long format
     df_long = df.melt(
@@ -26,3 +28,8 @@ def data_normalization(csv_path: str) -> pd.DataFrame:
     df_long["antall"] = pd.to_numeric(df_long["antall"], errors="coerce")
 
     return df_long
+
+#if __name__ == "__main__":
+
+    #df = data_normalization("../data/ssb.csv")
+    #print("Kolonner:", df.columns.tolist())
